b165a71ef37d8bb66ebafa8b61cb1f52c5562e4a
Rajkumar Natarajan <raj.cs.psg@gmail.com>
2024-03-29 11:30:44 -0700

    made working

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use gpui::*;
 use assets::Assets;
 use settings::{default_settings, SettingsStore};
-use theme::{ThemeRegistry, ThemeSettings, LoadThemes};
+use theme::{ThemeRegistry, ThemeSettings, LoadThemes, ActiveTheme};
 use settings::Settings;
+use ui::prelude::*;
+use ui::Button;
 
 struct HelloWorld {
     text: SharedString,
+    count: i32
 }
  
 impl Render for HelloWorld {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let button = Button::new("test-id", "Click Me")
+        .style(ui::ButtonStyle::Filled)
+        .size(ui::ButtonSize::Large)
+        .on_click(_cx.listener(move |this: &mut HelloWorld, selection: &ClickEvent, cx: &mut ViewContext<'_, HelloWorld>| {
+            this.count += 1;
+        }));
+
         div()
             .flex()
-            //.bg(rgb(0xfffff5))
+            .flex_col()
             .bg(_cx.theme().colors().background)
             .size_full()
             .justify_center()
             .items_center()
             .text_xl()
-            .text_color(_cx.text_style().color)
-            .child(format!("Hello, {}!", &self.text))
+            .text_color(_cx.theme().colors().text)
+            .child(format!("Hello, {}!", &self.count))
+            .child(button)
     }
 }
  
@@ -38,12 +49,16 @@ fn main() {
         let theme_registry  = ThemeRegistry::global(cx);
         let mut theme_settings = ThemeSettings::get_global(cx).clone();
         theme_settings.active_theme = theme_registry.get(&theme_name).unwrap();
+
+        let theme_names: Vec<SharedString> = theme_registry.list_names(true);
+        println!("theme_names: {:?}", theme_names);
         ThemeSettings::override_global(theme_settings, cx);
         
 
         cx.open_window(WindowOptions::default(), |cx| {
             cx.new_view(|_cx| HelloWorld {
                 text: "World".into(),
+                count: 0,
             })
         });
     });
