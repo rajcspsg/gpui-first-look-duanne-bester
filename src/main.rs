c0474259dad94dfa7ed8114f5199fbde1aec1eef
Rajkumar Natarajan <raj.cs.psg@gmail.com>
2024-03-28 19:42:06 -0700

    method_theme_not_found

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use gpui::*;
 use assets::Assets;
 use settings::{default_settings, SettingsStore};
-use theme::{ThemeRegistry, ThemeSettings};
-use seetings::Settings;
+use theme::{ThemeRegistry, ThemeSettings, LoadThemes};
+use settings::Settings;
 
 struct HelloWorld {
     text: SharedString,
@@ -12,12 +12,13 @@ impl Render for HelloWorld {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .flex()
-            .bg(rgb(0x2e7d32))
+            //.bg(rgb(0xfffff5))
+            .bg(_cx.theme().colors().background)
             .size_full()
             .justify_center()
             .items_center()
             .text_xl()
-            .text_color(rgb(0xffffff))
+            .text_color(_cx.text_style().color)
             .child(format!("Hello, {}!", &self.text))
     }
 }
@@ -32,12 +33,13 @@ fn main() {
             .unwrap();
         cx.set_global(store);
         
-        theme::init(themes_to_load: theme::LoadThemes::All(Box::new(Assets)), cx);
+        theme::init(LoadThemes::All(Box::new(Assets)), cx );
 
-        let theme_registry :&ThemeRegistry = cx.global::<ThemeRegistry>();
-        let mut theme_settings: ThemeSettings = ThemeSettings::get_global(cx).clone();
+        let theme_registry  = ThemeRegistry::global(cx);
+        let mut theme_settings = ThemeSettings::get_global(cx).clone();
         theme_settings.active_theme = theme_registry.get(&theme_name).unwrap();
         ThemeSettings::override_global(theme_settings, cx);
+        
 
         cx.open_window(WindowOptions::default(), |cx| {
             cx.new_view(|_cx| HelloWorld {
